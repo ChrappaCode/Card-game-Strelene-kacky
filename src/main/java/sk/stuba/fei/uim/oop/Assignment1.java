18ff38469e8af4e1036c53425f59e65bc737ac5d
Jakub <xchrappaj@stuba.sk>
2022-03-05 14:09:19 +0100

    AKCNE KARTY

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -6,7 +6,6 @@ import sk.stuba.fei.uim.oop.streleneKacky.StreleneKacky;
 public class Assignment1 {
     public static void main(String[] args) {
 
-
         StreleneKacky streleneKacky = new StreleneKacky();
 
     }
--- src/main/java/sk/stuba/fei/uim/oop/karty/Karty.java
+++ src/main/java/sk/stuba/fei/uim/oop/karty/Karty.java
@@ -1,4 +1,4 @@
-package sk.stuba.fei.uim.oop;
+package sk.stuba.fei.uim.oop.karty;
 
 import sk.stuba.fei.uim.oop.hrac.Hrac;
 
