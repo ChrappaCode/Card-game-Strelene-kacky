c3d84309bdec672790e7bc84b10fff3128f6b97e
Jakub <xchrappaj@stuba.sk>
2022-03-18 14:07:10 +0100

    vizual upgrades

--- src/main/java/sk/stuba/fei/uim/oop/hraciepole/Kacka.java
+++ src/main/java/sk/stuba/fei/uim/oop/hraciepole/Kacka.java
@@ -4,7 +4,7 @@ import sk.stuba.fei.uim.oop.hrac.Hrac;
 
 public class Kacka extends HraciePole{
 
-    private static final String MENO = "\uD83E\uDD86";
+    private static final String MENO = "\uD83E\uDDA2";
 
     public Kacka(Hrac hracovaKacka) {
         super(MENO, hracovaKacka.getMeno(), hracovaKacka.getPoradoveCislo());
--- src/main/java/sk/stuba/fei/uim/oop/hraciepole/Voda.java
+++ src/main/java/sk/stuba/fei/uim/oop/hraciepole/Voda.java
@@ -2,7 +2,7 @@ package sk.stuba.fei.uim.oop.hraciepole;
 
 public class Voda extends HraciePole{
 
-    private static final String MENO = "Voda";
+    private static final String MENO = "\uD83E\uDDCA Voda";
 
     public Voda() {
         super(MENO,"" , 0);
